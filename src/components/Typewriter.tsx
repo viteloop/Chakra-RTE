0f7d179e54388226522827235fcfd0e21cd19406
Frederic Lang <frederic1.lang.ext@orange.fr>
2022-07-01 19:37:27 +0200

    first commit

---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export interface TypewriterProps {
+}
+
+const Typewriter = (props: TypewriterProps) => {
+  return <div>Hello World</div>;
+};
+
+export default Typewriter;
